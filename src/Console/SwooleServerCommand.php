af86464132e66573b801d00a5fe5745c79de7b82
NickYuan <nick@NickYuandeMacBook-Pro.local>
2020-03-11 09:27:22 +0800

    update

--- src/Console/SwooleServerCommand.php
+++ src/Console/SwooleServerCommand.php
@@ -29,24 +29,5 @@ class SwooleServerCommand extends Command
     public function handle()
     {
         WebSocket::run();
-
-//        $ws = new Server("0.0.0.0", 9502);
-//        //监听WebSocket连接打开事件
-//        $ws->on('open', function ($ws, $request) {
-//            $ws->push($request->fd, "hello, welcome\n");
-//        });
-//
-//        //监听WebSocket消息事件
-//        $ws->on('message', function ($ws, $frame) {
-//            echo $frame->fd . "---Message: {$frame->data}\n";
-//            $ws->push($frame->fd, "server: {$frame->data}");
-//        });
-//
-//        //监听WebSocket连接关闭事件
-//        $ws->on('close', function ($ws, $fd) {
-//            echo "client-{$fd} is closed\n";
-//        });
-//        $ws->start();
-
     }
 }
